5ddefe02e0e07fde642ad69057d80e19507e024d
jane <janeluciaschoenfeld@gmail.com>
2025-01-02 18:58:34 +0100

    turn them into classes

--- oscbrick/oschandler/pentagon_control.py
+++ oscbrick/oschandler/pentagon_control.py
@@ -1,76 +1,98 @@
 from threading import Thread
-
-from oscbrick.oscbrick import EV3
-from oscbrick.config import Config
 from oscbrick.oscsender import Sender, construct_path
 
-from pybricks.parameters import Color
-
-from oscbrick.pentagon.movement import turn_left, turn_right, turn_around, drive_forward, drive_according_to_list, \
-    STANDARD_DRIVE_DISTANCE, scan
-
-from oscbrick.utilities import run_in_thread
-
+from oscbrick.pentagon.movement import RobotController  # Importiere die Klasse
 
 def get_handler():
     return PentagonControlHandler()
 
 
 class PentagonControlHandler:
+    def __init__(self):
+        self.robot_controller = RobotController()  # Instanz der RobotController-Klasse
+
     def handle(self, path, types_of_args, args):
         if path[0] == 'pentagon':
             print("Handling pentagon control")
-            # Drive
-            if len(path) > 1 and path[1] == 'drive':
-                print("pentagon drive")
-                if len(path) > 2 and path[2] == 'forward':
-                    print("pentagon drive forward")
-                    if len(args) == 0:
-                        run_in_thread(drive_forward, STANDARD_DRIVE_DISTANCE)
-                        Sender.send(construct_path('pentagon', 'response', 'drive', 'forward', 'done'))
-                    elif len(args) == 1 and str(types_of_args) == 'i':
-                        run_in_thread(drive_forward, int(args[0]))
-                        Sender.send(construct_path('pentagon', 'response', 'drive', 'forward', 'done'))
-                    pass
-                elif len(path) > 2 and path[2] == 'backward':
-                    print("pentagon drive backward")
-                    if len(args) == 0:
-                        run_in_thread(drive_forward, -STANDARD_DRIVE_DISTANCE)
-                        Sender.send(construct_path('pentagon', 'response', 'drive', 'backward', 'done'))
-                    elif len(args) == 1 and str(types_of_args) == 'i':
-                        run_in_thread(drive_forward, -int(args[0]))
-                        Sender.send(construct_path('pentagon', 'response', 'drive', 'backward', 'done'))
-                    pass
-                pass
-            elif len(path) > 1 and path[1] == 'turn':
-                print("pentagon turn")
-                if len(path) > 2 and path[2] == 'left':
-                    print("pentagon turn left")
-                    run_in_thread(turn_left)
-                    Sender.send(construct_path('pentagon', 'response', 'turn', 'left', 'done'))
-                elif len(path) > 2 and path[2] == 'right':
-                    print("pentagon turn right")
-                    run_in_thread(turn_right)
-                    Sender.send(construct_path('pentagon', 'response', 'turn', 'right', 'done'))
-                elif len(path) > 2 and path[2] == 'around':
-                    print("pentagon turn around")
-                    run_in_thread(turn_around)
-                    Sender.send(construct_path('pentagon', 'response', 'turn', 'around', 'done'))
-                pass
-            elif len(path) > 1 and path[1] == 'scan':
-                print("pentagon scan")
+            try:
+                # Drive
+                if len(path) > 1 and path[1] == 'drive':
+                    print("pentagon drive")
+                    if len(path) > 2 and path[2] == 'forward':
+                        print("pentagon drive forward")
+                        try:
+                            if len(args) == 0:
+                                self.robot_controller.drive_forward(self.robot_controller.STANDARD_DRIVE_DISTANCE)
+                                Sender.send(construct_path('pentagon', 'response', 'drive', 'forward', 'done'))
+                            elif len(args) == 1 and str(types_of_args) == 'i':
+                                self.robot_controller.drive_forward(int(args[0]))
+                                Sender.send(construct_path('pentagon', 'response', 'drive', 'forward', 'done'))
+                        except Exception as e:
+                            print("Drive Forward Error: {0}".format(e))
+                            Sender.send(construct_path('pentagon', 'response', 'drive', 'forward', 'error'))
 
-                try:
-                    result = scan()
-                    Sender.send(construct_path('pentagon', 'response', 'scan', 'done'), result.get("distance_r"),
-                            result.get("distance_m"), result.get("distance_l"), str(result.get("color")))
-                except:
-                    print("ERROR")
-                    return
-                
-            elif len(path) > 1 and path[1] == 'runList':
-                print("pentagon runList")
-                if len(args) > 0:
-                    run_in_thread(drive_according_to_list,args)
-                    Sender.send(construct_path('pentagon', 'response', 'runList', 'done'))
-                pass
+                    elif len(path) > 2 and path[2] == 'backward':
+                        print("pentagon drive backward")
+                        try:
+                            if len(args) == 0:
+                                self.robot_controller.drive_forward(-self.robot_controller.STANDARD_DRIVE_DISTANCE)
+                                Sender.send(construct_path('pentagon', 'response', 'drive', 'backward', 'done'))
+                            elif len(args) == 1 and str(types_of_args) == 'i':
+                                self.robot_controller.drive_forward(-int(args[0]))
+                                Sender.send(construct_path('pentagon', 'response', 'drive', 'backward', 'done'))
+                        except Exception as e:
+                            print("Drive Backward Error: {0}".format(e))
+                            Sender.send(construct_path('pentagon', 'response', 'drive', 'backward', 'error'))
+
+                elif len(path) > 1 and path[1] == 'turn':
+                    print("pentagon turn")
+                    if len(path) > 2 and path[2] == 'left':
+                        print("pentagon turn left")
+                        try:
+                            self.robot_controller.turn_left()
+                            Sender.send(construct_path('pentagon', 'response', 'turn', 'left', 'done'))
+                        except Exception as e:
+                            print("Turn Left Error: {0}".format(e))
+                            Sender.send(construct_path('pentagon', 'response', 'turn', 'left', 'error'))
+
+                    elif len(path) > 2 and path[2] == 'right':
+                        print("pentagon turn right")
+                        try:
+                            self.robot_controller.turn_right()
+                            Sender.send(construct_path('pentagon', 'response', 'turn', 'right', 'done'))
+                        except Exception as e:
+                            print("Turn Right Error: {0}".format(e))
+                            Sender.send(construct_path('pentagon', 'response', 'turn', 'right', 'error'))
+
+                    elif len(path) > 2 and path[2] == 'around':
+                        print("pentagon turn around")
+                        try:
+                            self.robot_controller.turn_around()
+                            Sender.send(construct_path('pentagon', 'response', 'turn', 'around', 'done'))
+                        except Exception as e:
+                            print("Turn Around Error: {0}".format(e))
+                            Sender.send(construct_path('pentagon', 'response', 'turn', 'around', 'error'))
+
+                elif len(path) > 1 and path[1] == 'scan':
+                    print("pentagon scan")
+                    try:
+                        result = self.robot_controller.scan()
+                        Sender.send(construct_path('pentagon', 'response', 'scan', 'done'), result.get("distance_r"),
+                                    result.get("distance_m"), result.get("distance_l"), str(result.get("color")))
+                    except Exception as e:
+                        print("Scan Error: {0}".format(e))
+                        Sender.send(construct_path('pentagon', 'response', 'scan', 'error'))
+
+                elif len(path) > 1 and path[1] == 'runList':
+                    print("pentagon runList")
+                    try:
+                        if len(args) > 0:
+                            self.robot_controller.drive_according_to_list(args)
+                            Sender.send(construct_path('pentagon', 'response', 'runList', 'done'))
+                    except Exception as e:
+                        print("RunList Error: {0}".format(e))
+                        Sender.send(construct_path('pentagon', 'response', 'runList', 'error'))
+
+            except Exception as e:
+                print("Unhandled Error: {0}".format(e))
+                Sender.send(construct_path('pentagon', 'response', 'error'))
--- oscbrick/pentagon/movement.py
+++ oscbrick/pentagon/movement.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 from pybricks.tools import wait
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port, Stop, Direction, Color
@@ -6,209 +8,207 @@ from pybricks.robotics import DriveBase
 from oscbrick.pentagon.scanner import get_color, get_distance
 from oscbrick.utilities import run_in_thread
 
-STANDARD_DRIVE_DISTANCE = 300
+class RobotController:
+    STANDARD_DRIVE_DISTANCE = 300
 
-class LookingDirection:
-    directions = ["NORTH", "EAST", "SOUTH", "WEST"]
+    def __init__(self):
+        self.motor_left = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
+        self.motor_right = Motor(Port.D)
+        self.motor_neck = Motor(Port.B)
 
-    def __init__(self, direction: str = None):
-        if direction:
-            self.current_direction = direction
-        else:
-            self.current_direction = "NORTH"
+        self.initRobot()
+
+        self.skip_next_instruction = False
+        self.looking_direction = self.LookingDirection()
+
+    def initRobot(self):
+        self.robot = DriveBase(self.motor_left, self.motor_right, wheel_diameter=56, axle_track=47.7)
+        self.robot.settings(100, 50, 90, 180)
+
+    class LookingDirection:
+        directions = ["NORTH", "EAST", "SOUTH", "WEST"]
+
+        def __init__(self, direction: str = None):
+            self.current_direction = direction if direction else "NORTH"
+
+        def turn_right(self):
+            idx_next = (self.directions.index(self.current_direction) + 1) % len(self.directions)
+            self.current_direction = self.directions[idx_next]
+
+        def turn_left(self):
+            idx_next = (self.directions.index(self.current_direction) - 1) % len(self.directions)
+            self.current_direction = self.directions[idx_next]
+
+        def turn_around(self):
+            idx_next = (self.directions.index(self.current_direction) + 2) % len(self.directions)
+            self.current_direction = self.directions[idx_next]
 
     def turn_right(self):
-        directions = self.directions
-        idx_next = (directions.index(self.current_direction) + 1) % len(directions)
-        self.current_direction = directions[idx_next]
+        self.initRobot()
+        self.looking_direction.turn_right()
+        print("Turning right!")
+        self.robot.stop()
+        self.robot.turn(90)
 
     def turn_left(self):
-        directions = self.directions
-        idx_next = (directions.index(self.current_direction) - 1) % len(directions)
-        self.current_direction = directions[idx_next]
+        self.initRobot()
+        self.looking_direction.turn_left()
+        print("Turning left!")
+        self.robot.stop()
+        self.robot.turn(-90)
 
     def turn_around(self):
-        directions = self.directions
-        idx_next = (directions.index(self.current_direction) + 2) % len(directions)
-        self.current_direction = directions[idx_next]
+        self.initRobot()
+        self.looking_direction.turn_around()
+        print("Turning around!")
+        self.robot.stop()
+        self.robot.turn(180)
 
+    def spin(self):
+        self.initRobot()
+        print("You spin my head right round right round")
+        self.robot.stop()
+        self.robot.turn(360)
 
-motor_left = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
-motor_right = Motor(Port.D)
-
-motor_neck = Motor(Port.B)
-
-looking_direction = LookingDirection()
-skip_next_instruction = False
-
-robot = DriveBase(motor_left, motor_right, wheel_diameter=56, axle_track=47.7)
-print("Base Settings:", robot.settings())
-robot.settings(100, 50, 90, 180)
-print("New Settings:", robot.settings())
-
-base_speed = 100
-
-
-def turn_right():
-    looking_direction.turn_right()
-    print("Turning right!")
-    robot.stop()
-    robot.turn(90)
-
-
-def turn_left():
-    looking_direction.turn_left()
-    print("Turning left!")
-    robot.stop()
-    robot.turn(-90)
-
-
-def turn_around():
-    looking_direction.turn_around()
-    print("Turning around!")
-    robot.stop()
-    robot.turn(180)
-
-
-def spin():
-    print("You spin my head right round right round")
-    robot.stop()
-    robot.turn(360)
-
-
-def drive_forward(distance: int, is_check: bool = True, next_instruction: str = None):
-    robot.stop()
-    robot.straight(distance)
-    if is_check:
-        wait(1000)
-        # check_alignment(next_instruction)
-
-
-def align_forwards():
-    print("Align Forwards")
-    robot.stop()
-
-    run_in_thread(motor_right.run_until_stalled, 100, Stop.BRAKE, 22)
-    run_in_thread(motor_left.run_until_stalled, 100, Stop.BRAKE, 22)
-    wait(1000)
-    while (motor_right.speed() > 0 or motor_left.speed() > 0):
-        pass
-    wait(1000)
-    drive_forward(-40, False)
-
-
-def align_backwards():
-    print("Align Backwards")
-    robot.stop()
-
-    run_in_thread(motor_right.run_until_stalled, -100, Stop.BRAKE, 23)
-    run_in_thread(motor_left.run_until_stalled, -100, Stop.BRAKE, 23)
-    wait(1000)
-    while (motor_right.speed() < 0 or motor_left.speed() < 0):
-        pass
-    wait(1000)
-    drive_forward(40, False)
-
-
-def align_neck(isRight: bool = True):
-    robot.stop()
-    print("aligning the neck")
-    motor_neck.reset_angle(0)
-    if isRight:
-        motor_neck.run_until_stalled(-200, Stop.COAST, 40)
-    else:
-        motor_neck.run_until_stalled(200, Stop.COAST, 40)
-    motor_neck.reset_angle(0)
-
-
-def scan() -> dict[str, int | Color]:
-    color = get_color()
-
-    align_neck(True)
-    # Scan right
-    r_distance = get_distance()
-    wait(1000)
-    # Turn left (to middle) and scan
-    motor_neck.run_angle(200, 105)
-    wait(1000)
-    m_distance = get_distance()
-    wait(1000)
-    # Turn left (to left)
-    align_neck(False)
-    l_distance = get_distance()
-
-    result = {"distance_r": r_distance, "distance_m": m_distance, "distance_l": l_distance, "color": color}
-
-    print(result)
-
-    return result
-
-
-def check_alignment(next_instruction: str = ""):
-    global skip_next_instruction
-    scan_result = scan()
-    print("Checking Alignment")
-    print(scan_result)
-
-    print("Alignment checker: The next instruction is {}".format(next_instruction))
-
-    # always align to the front if possible
-    if scan_result.get("distance_m") < 140:
-        align_forwards()
-        wait(2000)
-
-    if scan_result.get("distance_r") < 140:
-        turn_left()
-        wait(1000)
-        align_backwards()
-
-        if next_instruction != "turn_left":
+    def drive_forward(self, distance: int, is_check: bool = True, next_instruction: str = None):
+        self.initRobot()
+        self.robot.stop()
+        self.robot.straight(distance)
+        
+        if is_check:
             wait(1000)
-            turn_right()
-        else:
-            print("test")
-            skip_next_instruction = True
+            self.check_alignment(next_instruction)
 
-    elif scan_result.get("distance_l") < 140:
-        turn_right()
+    def align_forwards(self):
+        self.initRobot()
+
+        print("Align Forwards")
+        self.robot.stop()
+
+        run_in_thread(self.motor_right.run_until_stalled, 100, Stop.BRAKE, 22)
+        run_in_thread(self.motor_left.run_until_stalled, 100, Stop.BRAKE, 22)
         wait(1000)
-        align_backwards()
+        while (self.motor_right.speed() > 0 or self.motor_left.speed() > 0):
+            pass
+        wait(1000)
+        self.drive_forward(-40, False)
 
-        if next_instruction != "turn_right":
+    def align_backwards(self):
+        self.initRobot()
+        print("Align Backwards")
+        self.robot.stop()
+
+        run_in_thread(self.motor_right.run_until_stalled, -100, Stop.BRAKE, 23)
+        run_in_thread(self.motor_left.run_until_stalled, -100, Stop.BRAKE, 223)
+        wait(1000)
+        while (self.motor_right.speed() > 0 or self.motor_left.speed() > 0):
+            pass
+        wait(1000)
+        self.drive_forward(40, False)
+
+    def align_neck(self, is_right: bool = True):
+        self.initRobot()
+        self.robot.stop()
+        print("Aligning the neck")
+        self.motor_neck.reset_angle(0)
+        if is_right:
+            self.motor_neck.run_until_stalled(-200, Stop.BRAKE, 40)
+        else:
+            self.motor_neck.run_until_stalled(200, Stop.BRAKE, 40)
+        self.motor_neck.reset_angle(0)
+    
+    def _get_consistent_distance(self):
+        distance_1 = get_distance()
+        print("Distance 1: {}".format(distance_1))
+        wait(500)
+        distance_2 = get_distance()
+        print("Distance 2: {}".format(distance_2))
+        if distance_1 != distance_2:
+            print("Distance values are inconsistent, rescanning...")
+            return get_distance()
+        return distance_1
+
+    def scan(self):
+        color = get_color()
+
+        self.align_neck(True)
+        wait(500)
+        r_distance = self._get_consistent_distance()
+        print("Distance r: {}".format(r_distance))
+
+        self.motor_neck.run_angle(200, 115)
+        wait(500)
+        m_distance = self._get_consistent_distance()
+        print("Distance m: {}".format(m_distance))
+
+
+
+        self.align_neck(False)
+        l_distance = self._get_consistent_distance()
+        print("Distance l: {}".format(l_distance))
+
+
+        result = {
+            "distance_r": r_distance,
+            "distance_m": m_distance,
+            "distance_l": l_distance,
+            "color": color
+        }
+
+        print(result)
+        return result
+
+    def check_alignment(self, next_instruction: str = ""):
+        self.initRobot()
+        scan_result = self.scan()
+        print("Checking Alignment")
+        print(scan_result)
+
+        if scan_result.get("distance_m") < 140:
+            self.align_forwards()
+            wait(2000)
+
+        if scan_result.get("distance_r") < 140:
+            self.turn_left()
             wait(1000)
-            turn_left()
-        else:
-            skip_next_instruction = True
+            self.align_backwards()
+            if next_instruction != "turn_left":
+                wait(1000)
+                self.turn_right()
+            else:
+                self.skip_next_instruction = True
 
+        elif scan_result.get("distance_l") < 140:
+            self.turn_right()
+            wait(1000)
+            self.align_backwards()
+            if next_instruction != "turn_right":
+                wait(1000)
+                self.turn_left()
+            else:
+                self.skip_next_instruction = True
 
-def drive_according_to_list(instructions: dict[str]):
-    global skip_next_instruction
+    def drive_according_to_list(self, instructions):
+        self.initRobot()
+        print("Driving according to instruction list")
+        for idx, instruction in enumerate(instructions):
+            print("Step: {}: {}".format(idx + 1, instruction))
 
-    print("Driving according to instruction list {}".format(instructions))
-    for idx, instruction in enumerate(instructions):
-        print("Step: {} of {}: {}".format(idx + 1, len(instructions), instruction))
+            if self.skip_next_instruction:
+                self.skip_next_instruction = False
+                print("Skipping step: '{}' because it was already fulfilled".format(instruction))
+                continue
 
-        if skip_next_instruction:
-            skip_next_instruction = False
-            print("Skipping step: '{}' because it was already fulfilled".format(instruction))
-            continue
+            if instruction == "drive_forward":
+                self.drive_forward(self.STANDARD_DRIVE_DISTANCE)
 
-        next_instruction = None
+            elif instruction == "turn_around":
+                self.turn_around()
 
-        if idx + 1 < len(instructions):
-            next_instruction = instructions[idx + 1]
+            elif instruction == "turn_left":
+                self.turn_left()
 
-        if instruction == "drive_forward":
-            drive_forward(STANDARD_DRIVE_DISTANCE, next_instruction=next_instruction)
+            elif instruction == "turn_right":
+                self.turn_right()
 
-        if instruction == "turn_around":
-            turn_around()
-
-        if instruction == "turn_left":
-            turn_left()
-
-        if instruction == "turn_right":
-            turn_right()
-
-        # wait 1s after each procedure
-        wait(1000)
+            wait(1000)
